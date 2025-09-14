2c8335a45105d22ef72f325998abffb2eeb49414
Faiz1401 <faizrazak1401@gmail.com>
2025-09-14 16:57:08 +0800

    Update n je

--- db_connection.php
+++ db_connection.php
@@ -3,7 +3,7 @@ $servername = "localhost";    // Database server (use localhost if running local
 $username = "root";           // Your database username
 $password = "";               // Your database password
 $dbname = "jppkk_test";       // Your database name
-$port = 4306;                 // Custom port for MySQL connectio
+$port = 4306;                 // Custom port for MySQL connectionn
 
 // Create connection using custom port
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
